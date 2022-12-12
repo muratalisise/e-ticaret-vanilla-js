aa0073ab4e6581670d5c6f65c2d9fb60c5118384
Murat Ali Şişe <92972964+muratalisise@users.noreply.github.com>
2022-12-12 17:14:30 +0300

    12.12.2022

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const toggleModal = () => {
     basketModelEl.classList.toggle("active");
 }
 
+
     const getBooks = () => {
-        fetch("./products.json")
-        .then((res) => res.json())
-        .then((books) => (bookList = books));
+      fetch("./products.json")
+      .then((res) => res.json())
+      .then((books) => (bookList = books));
     };
-  getBooks();
-  
+    getBooks();
 
 
     const createBookStars = (starRate) => {
@@ -26,44 +26,42 @@ const toggleModal = () => {
         return starRateHtml;
     };
 
-
-
-  const createBookItemHtml = () => {
-    const bookListEl = document.querySelector(".book_list");
-    let bookListHtml = "";
-    bookList.forEach((book, index) => {
-        bookListHtml += 
-        `
-            <div class="col-5 ${index % 2 == 0 && "offset-2"} my-5">
-              <div class="row book_card">
-                <div class="col-6">
-                    <img class="img-fluid shadow" width="258" height="400" src="${book.imgSource}">
-                </div>
-                <div class="col-6">
-                    <div class="book-detail">
-                        <span class="fos gray fs-5">${book.author}</span> <br />
-                        <span class="fs-4 fw-bold">${book.name}</span> <br />
-                        <span class="book_star_rate"> 
-                            ${createBookStars(book.starRate)}
-                          <span class="gray">${book.reviewCount}</span>
-                        </span>
-                    </div>
-                   <p class="book-description fos gray">
-                        ${book.description}
-                   </p>
-                    <div>
-                      <span class="black fw-bold fs-4 me-2"> ${book.price} TL</span>
-                      ${book.oldPrice && `<span class="fs-4 fw-bold old_price"> ${book.oldPrice} TL</span>` }
-                    </div>
-                    <button class="btn btn-outline-success">Sepete Ekle</button>
+    const createBookItemHtml = () => {
+      const bookListEl = document.querySelector(".book_list");
+      let bookListHtml = "";
+      bookList.forEach((book, index) => {
+          bookListHtml += 
+          `
+              <div class="col-5 ${index % 2 == 0 && "offset-2"} my-5">
+                <div class="row book_card">
+                  <div class="col-6">
+                      <img class="img-fluid shadow" width="258" height="400" src="${book.imgSource}">
+                  </div>
+                  <div class="col-6">
+                      <div class="book-detail">
+                          <span class="fos gray fs-5">${book.author}</span> <br />
+                          <span class="fs-4 fw-bold">${book.name}</span> <br />
+                          <span class="book_star_rate"> 
+                              ${createBookStars(book.starRate)}
+                            <span class="gray">${book.reviewCount}</span>
+                          </span>
+                      </div>
+                    <p class="book-description fos gray">
+                          ${book.description}
+                    </p>
+                      <div>
+                        <span class="black fw-bold fs-4 me-2"> ${book.price} TL</span>
+                        ${book.oldPrice && `<span class="fs-4 fw-bold old_price"> ${book.oldPrice} TL</span>` }
+                      </div>
+                      <button class="btn btn-outline-success">Sepete Ekle</button>
+                  </div>
                 </div>
               </div>
-            </div>
-        `
-        ; 
-    });
-    bookListEl.innerHTML += bookListHtml;
-  };
+          `
+          ; 
+      });
+      bookListEl.innerHTML += bookListHtml;
+    };
 
 
         createBookItemHtml();
